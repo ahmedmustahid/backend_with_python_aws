f66f46d43f71a1ba106f048fffa33f5a12c92c2e
ahmedmustahid <einsteinium25@gmail.com>
2025-06-12 11:27:23 +0900

    server-client send-recv messages

--- tcp_server.py
+++ tcp_server.py
@@ -7,8 +7,28 @@ sock.bind((host, port))
 
 print(f"server running at http://{host}:{port}")
 try:
+    sock.listen(2)
     while True:
-        sock.listen()
+        conn, addr = sock.accept()
+        print(f"connected to {addr}")
+        ret_data = conn.recv(1024)
+        if not ret_data:
+            break
+        ret_string = ret_data.decode()
+        print(f"client says: {ret_string}")
+
+        # Build a minimal HTTP response
+        body = f"Echo: {ret_string}"
+        response = (
+            "HTTP/1.1 200 OK\r\n"
+            f"Content-Length: {len(body)}\r\n"  # Tell curl how much data to expect
+            "Content-Type: text/plain\r\n"
+            "\r\n"  # End of headers
+            f"{body}"
+        )
+        conn.sendall(response.encode())
+        conn.close()  # Close the connection (optional if Content-Length is set)
 except KeyboardInterrupt:
-    print("cleaning up")
+    print("shutting down...")
+finally:
     sock.close()
